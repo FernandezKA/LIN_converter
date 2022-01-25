babbed685bc386d21706132b10a476c2411079ea
FernandezKA <fernandes.kir@yandex.ru>
2022-01-25 18:02:00 +0300

    Deleted packet size at RS232 reflect packet

--- src/communication.c
+++ src/communication.c
@@ -9,7 +9,7 @@ enum PartDigit ValueReceived = value_completed;
 void Reflect_LIN(LIN_Header header, LIN_Response response)
 {
   Push(&sw_transmit, header.pid);
-  Push(&sw_transmit, header.size);
+  //Push(&sw_transmit, header.size);
   for (int i = 0; i < header.size; ++i)
   {
     Push(&sw_transmit, response.data[i]);
